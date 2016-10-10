83643905c9f40d1224f21f9892384236545c3248
xiyuan_fengyu <xiyuan_fengyu@163.com>
2016-10-10 17:12:45 +0800

    取消正则白名单过滤器，添加黑名单正则表达式

--- src/main/scala/com/xiyuan/Example.scala
+++ src/main/scala/com/xiyuan/Example.scala
@@ -10,10 +10,13 @@ import com.xiyuan.template.log.XYLog
 object Example {
 
   def main(args: Array[String]) {
-    val str = "长安镇长盛小姐找可以联系包夜电话信息"
-
+    val str = "怎么查询删除别人的通话记录清单怎么查询删除别人的通话记录清单，请找扣33722577.找他不错，希望帮到大家！\n    皇后人选是先帝钦定，冯家千挑万捡送进来一个冯卉，他的母后更早早存了别的心思。总归是亲娘，没闹出什么，他也只当没那么回事，但冯家想再出来一个冯太后是不可能的。\n    两人的心思都转个不停，章煜却没有话想说，他也还不十分习惯与宋淑好这样身份的人有太多的交流，并找不到共同话题。"
+    DefaultMsgChecker.isDirty("")
+    println("加载完毕")
     //可以使用多层过滤系统
+    val now = System.currentTimeMillis()
     println(DefaultMsgChecker.isDirty(str))
+    println("耗时：" + (System.currentTimeMillis() - now))
 
     //也可以直接使用LetterMatrixFilter来判断，这种实现的正确率已经非常高了，上面的过滤系统最后一层过滤器就是使用的这个
     val letterMatrixFilter = new LetterMatrixFilter()
