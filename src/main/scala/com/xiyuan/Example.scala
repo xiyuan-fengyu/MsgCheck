0e9e9883780e444b0d419b6e5c2ad27880b83071
xiyuan_fengyu <xiyuan_fengyu@163.com>
2016-10-13 10:56:23 +0800

    耗时测试

--- src/main/scala/com/xiyuan/Example.scala
+++ src/main/scala/com/xiyuan/Example.scala
@@ -10,17 +10,22 @@ import com.xiyuan.template.log.XYLog
 object Example {
 
   def main(args: Array[String]) {
-    val str = "怎么查询删除别人的通话记录清单怎么查询删除别人的通话记录清单，请找扣33722577.找他不错，希望帮到大家！\n    皇后人选是先帝钦定，冯家千挑万捡送进来一个冯卉，他的母后更早早存了别的心思。总归是亲娘，没闹出什么，他也只当没那么回事，但冯家想再出来一个冯太后是不可能的。\n    两人的心思都转个不停，章煜却没有话想说，他也还不十分习惯与宋淑好这样身份的人有太多的交流，并找不到共同话题。"
+    val str = "怎么查询删除别人的通话记录清单怎么查询删除别人的通话记录清单，请找扣33722577.找他不错，希望帮到大家！\n    皇后人选是先帝钦定，冯家千挑万捡送进来一个冯卉，他的母后更早早存了别的心思。总归是亲娘，没闹出什么，他也只当没那么回事，但冯家想再出来一个冯太后是不可能的。\n    两人的心思都转个不停，章煜却没有话想说，他也还不十分习惯与宋淑好这样身份的人有太多的交流，并找不到共同话题。" +
+      "怎么查询删除别人的通话记录清单怎么查询删除别人的通话记录清单，请找扣33722577.找他不错，希望帮到大家！\n    皇后人选是先帝钦定，冯家千挑万捡送进来一个冯卉，他的母后更早早存了别的心思。总归是亲娘，没闹出什么，他也只当没那么回事，但冯家想再出来一个冯太后是不可能的。\n    两人的心思都转个不停，章煜却没有话想说，他也还不十分习惯与宋淑好这样身份的人有太多的交流，并找不到共同话题。"
     DefaultMsgChecker.isDirty("")
     println("加载完毕")
     //可以使用多层过滤系统
+    val num = 100000
     val now = System.currentTimeMillis()
-    println(DefaultMsgChecker.isDirty(str))
-    println("耗时：" + (System.currentTimeMillis() - now))
+    for (i <- 0 until num) {
+      DefaultMsgChecker.isDirty(str)
+    }
+    val time = System.currentTimeMillis() - now
+    println("耗时：" + time + ", 平均每条耗时：" + (time / num.toDouble))
 
-    //也可以直接使用LetterMatrixFilter来判断，这种实现的正确率已经非常高了，上面的过滤系统最后一层过滤器就是使用的这个
-    val letterMatrixFilter = new LetterMatrixFilter()
-    XYLog.d(str + "\n" + letterMatrixFilter.check(str))
+//    //也可以直接使用LetterMatrixFilter来判断，这种实现的正确率已经非常高了，上面的过滤系统最后一层过滤器就是使用的这个
+//    val letterMatrixFilter = new LetterMatrixFilter()
+//    XYLog.d(str + "\n" + letterMatrixFilter.check(str))
   }
 
 }
